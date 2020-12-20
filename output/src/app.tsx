2bdea603b44e46e6085ff193e324324c134c12b6
mox692 <55653825+mox692@users.noreply.github.com>
2020-12-20 19:51:14 +0900

    fdsaaaa

--- output/src/app.tsx
+++ output/src/app.tsx
@@ -15,6 +15,8 @@ export const AppRoute = () => {
          <Route path="/sub" component={SubPage} />
          <Route path="/sub" component={SubPage} />
          <Route path="/sub" component={SubPage} />
+         <Route path="/sub" component={SubPage} />
+         <Route path="/sub" component={SubPage} />
        </Switch>
   </HashRouter>,
   </>
